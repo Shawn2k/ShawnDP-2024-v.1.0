86f876d7fa532f3e71ef830ebf906d01895099d2
Sharon Mendez <83627856+Shawn2k@users.noreply.github.com>
2024-04-05 21:42:38 +0800

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -2,7 +2,7 @@
 net config server /srvcomment:"Windows Azure ni PHC-Zaxcal" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 net user runneradmin Wind0ws_explorer /add >nul
-curl -O https://raw.githubusercontent.com/Shawn2k/ShawnRDP2.0/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -O https://raw.githubusercontent.com/Shawn2k/ShawnRDP-2024-v.1.0/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
 echo This is personal VM of PHC Zaxcal
